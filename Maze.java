60fc1ea5af95ce9f4926f83ae2b9051caf0abec8
Japius <pmdu93@live.fr>
2018-02-01 11:59:06 +0100

    Case Start et End

--- Maze.java
+++ Maze.java
@@ -1,16 +1,18 @@
-import java.io.File;
+import java.io.*;
 import java.util.*;
 public class Maze{
 	private int[][] maze;
 	public final static int WALL = 0;
 	public final static int WAY = 1;
+	public final static int START = 2;
+	public final static int END = 3;
 
 	public Maze(int L, int l){
 		maze=new int[L][l];
 		randomMaze(L,l);
 	}
 
-	public Maze(File fic) throw FileNotFoundException{
+	public Maze(File fic) throws FileNotFoundException{
 		Scanner sc = new Scanner(fic);
 		ArrayList<String> tmp = new ArrayList<String>();
 		while(sc.hasNextLine())
@@ -19,7 +21,14 @@ public class Maze{
 		maze = new int[tmp.size()][max];
 		for(int i=0;i<tmp.size();i++){
 			for(int j=0;j<max;j++){
-				maze[i][j]=(tmp.get(i).charAt(j)==' '?WAY:WALL);
+				switch (tmp.get(i).charAt(j)){
+					case 'S':
+					maze[i][j]=START;break;
+					case ' ':
+					maze[i][j]=WAY;break;
+					default:
+					maze[i][j]=WALL;break;
+				}
 			}
 		}
 	}
@@ -33,10 +42,26 @@ public class Maze{
 		return res;
 	}
 
+	public static boolean sameLength(ArrayList<String> t){
+		int tmp = t.get(0).length();
+		for(String i:t){
+			if(i.length()!=tmp)
+				return false;
+		}
+		return true;
+	}
+
 	private void print(){
 		for(int i=0;i<maze.length;i++){
 			for(int j=0;j<maze[i].length;j++){
-				System.out.print((maze[i][j]==WAY?' ':'0'));
+				String tmp="";
+				switch(maze[i][j]){
+					case WAY: tmp=" ";break;
+					case START: tmp=" ";break;
+					case END: tmp=" ";break;
+					case WALL: tmp="\\";break;
+				}
+				System.out.print(tmp);
 			}
 			System.out.println( );
 		}
@@ -44,5 +69,15 @@ public class Maze{
 
 	private void randomMaze(int L,int l){
 
+    }
+
+    public static void main(String[] args){
+    	try{
+    		File fic = new File("labiTest.txt");
+    		Maze m = new Maze(fic);
+    		m.print();
+    	}catch(Exception e){
+    		//e.printStackTraceException();
+    	}
     }
 }
