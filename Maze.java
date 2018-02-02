b98fa1852d0de6d243108af877d5c15d97e01d0b
iligne42 <iman.lignel@gmail.com>
2018-02-02 09:27:28 +0100

    Creation de la fonction createBorder() et des fonctions dont elle a besoin. Début de la fonction récursive.

--- Maze.java
+++ Maze.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+import java.awt.*;
 public class Maze{
 	private int[][] maze;
 	public final static int WALL = 0;
@@ -7,9 +8,13 @@ public class Maze{
 	public final static int START = 2;
 	public final static int END = 3;
 
-	public Maze(int L, int l){
-		maze=new int[L][l];
-		randomMaze(L,l);
+	public Maze(int L, int l) throws FormatNotSupported{
+		if(L>3 && l>3) {
+            maze = new int[L][l];
+            randomMaze();
+        }else{
+		    throw new FormatNotSupported("Le labyrinthe est trop petit");
+        }
 	}
 
 	public Maze(File fic) throws FileNotFoundException, FormatNotSupported{
@@ -72,8 +77,74 @@ public class Maze{
 		}
 	}
 
-	private void randomMaze(int L,int l){
+	private void randomMaze(){
+        createBorder();
+    }
 
+    private void createBorder(){
+	    for(int i=0; i<maze.length; i++){
+	        for(int j=0; j<maze[i].length; j++){
+	            if(i==0||j==0||i==maze.length-1||j==maze[i].length-1) maze[i][j]=WALL;
+            }
+        }
+        Point Start=createStartEnd();
+	    setStartEnd(Start,START);
+	    Point End=createStartEnd();
+	    while(Start.equals(End)) End=createStartEnd();
+	    setStartEnd(End,END);
+    }
+
+    private Point createStartEnd(){
+	    Random rand=new Random();
+	    int VH=rand.nextInt(2);
+	    int i=0; int j=0;
+	    if(VH==0){
+	        i=rand.nextInt(2);
+	        if(i==1) i=maze.length-1;
+	        j=rand.nextInt(maze[i].length);
+        }else{
+	        i=rand.nextInt(maze.length);
+	        j=rand.nextInt(2);
+	        if(j==1) j=maze[i].length-1;
+        }
+        return new Point(i,j);
+    }
+
+    private void setStartEnd(Point p, int val){
+	    int i=(int)p.getX();
+	    int j=(int)p.getY();
+	    maze[i][j]=val;
+    }
+
+    private void createInside(){
+	    //Appeler les fonctions de création de murs vertical et horizontal
+    }
+
+    private void horizontal(int h){ //Commencer avec v=-1;
+	    Random rand=new Random();
+	    int i=rand.nextInt(maze.length-h);
+	    if(onAWall(i)) {
+            for (int j = 0; j < maze[i].length; j++) {
+                int tmp = rand.nextInt(maze[i].length - h);
+                if (j != tmp) maze[i][j] = WALL;
+                else maze[i][j] = WAY;
+            }
+        }else horizontal(h);
+    }
+
+    private void vertical(int v){
+	    Random rand=new Random();
+	    int j=rand.nextInt(v);//v?
+        for(int i=0; i<maze.length; i++){
+            int tmp=rand.nextInt(maze.length);
+            if(i!=tmp) maze[i][j]=WALL;
+            else maze[i][j]=WAY;
+        }
+    }
+
+    private boolean onAWall(int i){//vérifie qu'on créer le nouveau mur sur un mur et pas un "trou"
+	    if(maze[i][0]==WALL && maze[i][maze[i].length-1]==WALL) return true;
+	    else return false;
     }
 
     public static void main(String[] args){
