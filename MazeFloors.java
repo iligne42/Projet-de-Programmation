c4eed90f7bf1d0e170f74630ee05011892e0b6d0
iligne42 <iman.lignel@gmail.com>
2018-02-28 13:43:25 +0100

    Petite modification MazeFloors

--- MazeFloors.java
+++ MazeFloors.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.text.Normalizer;
 import java.util.*;
 import java.awt.*;
 public class MazeFloors{
@@ -10,19 +11,22 @@ public class MazeFloors{
     }
 
     public MazeFloors(int L, int l, int nb, int nbObstacles, int nbMonstres,int nbTeleport,int nbDoors,int nbBonus, int typeBonus) throws FormatNotSupported{
+        if(nb==0) throw new FormatNotSupported("You must put a number of floors, min 1");
         mazes=new LinkedList<Maze>();
         nbFloors=nb;
         while(nb!=0){
             mazes.add(new Maze(L,l,nbObstacles,nbMonstres,nbTeleport,nbDoors,nbBonus,typeBonus)); //problèmes dans getcase pour teleporteur et porte
             nb--;
         }
-        for(int i=0; i<mazes.size(); i++){
-            Maze m=mazes.get(i);
-            if(i==0) m.changeEnding();
-            else if(i==mazes.size()-1) m.changeBeginning();
-            else{
-                m.changeBeginning();
-                m.changeEnding();
+        if(nbFloors!=1) {
+            for (int i = 0; i < mazes.size(); i++) {
+                Maze m = mazes.get(i);
+                if (i == 0) m.changeEnding();
+                else if (i == mazes.size() - 1) m.changeBeginning();
+                else {
+                    m.changeBeginning();
+                    m.changeEnding();
+                }
             }
         }
     }
@@ -35,7 +39,7 @@ public class MazeFloors{
     }
 
     public static void main(String []args) throws FormatNotSupported{
-        MazeFloors mf=new MazeFloors(20,10,5,0,0,0,0,3,0);
+        MazeFloors mf=new MazeFloors(20,10,1,0,0,0,0,3,0);
         mf.print();
     }
 }
