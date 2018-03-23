85212bb1ba8c3d06d189a0b3a394428ecb0c1084
iligne42 <iman.lignel@gmail.com>
2018-03-23 09:03:28 +0100

    enlever com

--- Bonus.java
+++ Bonus.java
@@ -18,7 +18,7 @@ public abstract class Bonus extends Divers{
 
     public String getAvantage(){return avantage;}
 
-   /* public MeshView initBonus() throws IOException {
+   public MeshView initBonus() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader();
         PhongMaterial mat = new PhongMaterial();
         if(avantage.equals("Piece")) {
@@ -34,5 +34,5 @@ public abstract class Bonus extends Divers{
         bonus.setMaterial(mat);
         bonus.setRotationAxis(Rotate.Z_AXIS);
         return bonus;
-    }*/
+    }
 }
